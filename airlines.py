3f745d5c22bab8610935ad03a7c131d7216a3429
Muhammad Hadi <mhadijaz69@gmail.com>
2026-07-01 15:27:45 +0500

    Create airlines.py

---
+++ airlines.py
@@ -0,0 +1,22 @@
+airlines = {
+    "Emirates": [
+        "Airbus A380-800",
+        "Boeing 777-300ER"
+    ],
+
+    "Qatar Airways": [
+        "Airbus A350-1000",
+        "Boeing 787-9 Dreamliner"
+    ],
+
+    "British Airways": [
+        "Airbus A320",
+        "Boeing 787-9 Dreamliner",
+        "Airbus A380-800"
+    ],
+
+    "PIA": [
+        "Airbus A320",
+        "Boeing 777-200ER"
+    ]
+}
